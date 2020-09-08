c7ff0c79d09cc4c035e5f75bcb57e53f5ae96cb4
cybxtechnolab <cybxtechnolabs@gmail.com>
2020-09-08 16:52:26 +0530

    login msg

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,7 +21,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <b>Timestamp with Login</b>
+    <b>Login with Timestamp</b>
   </div>
   <!-- /.login-logo -->
   <div class="card">
